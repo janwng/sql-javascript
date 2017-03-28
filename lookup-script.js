d723b6696d937ac896567882652db7f0092e8658
Jan Wong <janwong94@gmail.com>
2017-03-28 20:55:09 +0000

    add lookup script, need to refactor

---
+++ lookup-script.js
@@ -0,0 +1,46 @@
+const pg = require("pg");
+const settings = require("./settings"); // settings.json
+
+const client = new pg.Client({
+  user     : settings.user,
+  password : settings.password,
+  database : settings.database,
+  host     : settings.hostname,
+  port     : settings.port,
+  ssl      : settings.ssl
+});
+
+// client.connect((err) => {
+//   if (err) {
+//     return console.error("Connection Error", err);
+//   }
+//   client.query("SELECT $1::int AS number", ["1"], (err, result) => {
+//     if (err) {
+//       return console.error("error running query", err);
+//     }
+//     console.log(result.rows[0].number); //output: 1
+//     client.end();
+//   });
+// });
+
+var input = process.argv[2];
+
+client.connect((err) => {
+  if (err) {
+    return console.error("Connection Error", err);
+  }
+
+  client.query("SELECT * FROM famous_people WHERE last_name = $1 OR first_name = $1", [input], (err, result, done) => {
+
+    done();
+
+    client.end();
+  });
+});
+
+function done() {
+  if (err) {
+    return console.error("error running query", err);
+  }
+  console.log(result.rows[0]);
+}

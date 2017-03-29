83786c74a6e7cd8f09b1fd3ec36baad9faa3bd90
Jan Wong <janwong94@gmail.com>
2017-03-29 02:32:25 +0000

    try out knex migration

---
+++ migrations/20170329002440_create_milestones.js
@@ -0,0 +1,29 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('milestones', function(table){
+      table.increments();
+      table.string('description');
+      table.date('date_achieved');
+    })
+  ])
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('milestones')
+  ])
+};
+
+
+//
+// exports.up = function(knex) {
+//   return knex.schema.createTable("milestones", (table) => {
+//     table.string("description");
+//     table.date("date_achieved");
+//   });
+// };
+//
+// exports.down = function(knex) {
+//   return knex.schema.dropTable("milestones");
+// }
